50f789c3524fe6296a74ecf11ec981cb7a97c1d7
Ponzi <43649035+Ponzis-Python@users.noreply.github.com>
2020-04-21 14:27:41 -0400

    Add files via upload

---
+++ runbashscr.py
@@ -0,0 +1,11 @@
+import os
+import subprocess
+import sys
+
+the_output = subprocess.check_output('ls -1 /Users/chumlee/fooDir/ | wc -l', shell=True)
+file_number = 20
+if int(the_output) < file_number:
+    print('fooDir is not mounted!!!')
+    sys.exit()
+    
+os.popen('sh /Users/chumlee/tmpdownload/cpfiles.sh')
